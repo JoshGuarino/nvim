96ae0a0444254f36b8074c6a950295831764d7a0
Josh Guarino <jguarino722@gmail.com>
2025-07-20 15:31:28 -0400

    lazy sync and lsp config cleanup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -14,25 +14,19 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+-- deno lsp custom config
 lspconfig.denols.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
   root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
 }
 
+-- ts_ls custom config
 lspconfig.ts_ls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
   root_dir = lspconfig.util.root_pattern "package.json",
   single_file_support = false,
 }
 
+-- gopls custom config
 lspconfig.gopls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
   cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
   settings = {
