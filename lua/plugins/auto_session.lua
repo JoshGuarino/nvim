178898b5493f60031c3327f1a9672cd1487bda30
Josh Guarino <jguarino722@gmail.com>
2025-08-05 14:22:55 -0400

    add auto-session plugin

---
+++ lua/plugins/auto_session.lua
@@ -0,0 +1,15 @@
+return {
+  "rmagatti/auto-session",
+  lazy = false,
+  keys = {
+    { "<leader>ss", "<cmd>SessionSearch<cr>" },
+    { "<leader>sw", "<cmd>SessionSave<cr>" },
+    { "<leader>sl", "<cmd>SessionLoad<cr>" },
+    { "<leader>sd", "<cmd>SessionDelete<cr>" },
+    { "<leader>sr", "<cmd>SessionRename<cr>" },
+    { "<leader>st", "<cmd>SessionToggleAutoSave<cr>" },
+  },
+  opts = {
+    suppressed_dirs = { "~/", "~/repos/", "/" },
+  },
+}
