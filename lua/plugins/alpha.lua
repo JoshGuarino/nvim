4986d12818dbca1bca8edf27fdca9125ddc68e23
Josh Guarino <jguarino722@gmail.com>
2025-12-22 16:12:50 -0500

    add alpha dashboard

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,29 @@
+return {
+	"goolord/alpha-nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = {
+			[[                                                    ]],
+			[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+			[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+			[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+			[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+			[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+			[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+			[[                                                    ]],
+		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button("ff", "󰮗   Find file", ":cd $HOME | Telescope find_files<CR>"),
+			dashboard.button("e", "   File Explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("fw", "󱘞   Ripgrep", ":Telescope live_grep<CR>"),
+			dashboard.button("q", "󰗼   Quit", ":qa<CR>"),
+		}
+
+		alpha.setup(dashboard.opts)
+	end,
+}
