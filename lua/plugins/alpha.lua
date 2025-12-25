c54a364106c514168d67a0cafef99ee94c134011
Josh Guarino <jguarino722@gmail.com>
2025-12-25 10:51:46 -0500

    update alpha buttons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -21,12 +21,12 @@ return {
 
 		dashboard.section.header.opts.hl = "AlphaHeader"
 		dashboard.section.buttons.val = {
-			dashboard.button("ff", "󰮗   Find file", ":cd $HOME | Telescope find_files<CR>"),
+			dashboard.button("f", "󰮗   Find file", ":cd $HOME | Telescope find_files<CR>"),
 			dashboard.button("e", "   File Explorer", "<cmd>NvimTreeToggle<CR>"),
 			dashboard.button("r", "󱘞   Ripgrep", ":Telescope live_grep<CR>"),
 			dashboard.button("q", "󰗼   Quit", ":qa<CR>"),
 			dashboard.button("l", "󰒲   Lazy", ":Lazy<CR>"),
-			dashboard.button("ch", "   Check Health", ":checkhealth<CR>"),
+			dashboard.button("c", "   Check Health", ":checkhealth<CR>"),
 			dashboard.button("m", "   Mason", ":Mason<CR>"),
 		}
 
