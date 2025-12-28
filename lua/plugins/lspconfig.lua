410dfa3c4739e558f71f455e68cbcedc9151ee8d
Josh Guarino <jguarino722@gmail.com>
2025-12-27 20:40:45 -0500

    add rust_analyzer to lsp config and conform cleanup

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ return {
 	dependencies = { "williamboman/mason.nvim" },
 	config = function()
 		-- enable lsps with default configs
-		vim.lsp.enable({ "lua_ls", "html", "cssls", "gopls", "pyright", "ts_ls", "denols" })
+		vim.lsp.enable({ "lua_ls", "html", "cssls", "gopls", "pyright", "ts_ls", "denols", "rust_analyzer" })
 
 		-- lua_ls custom config
 		vim.lsp.config("lua_ls", {
